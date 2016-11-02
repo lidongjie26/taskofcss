ba737f97fc2c6d1e9275298ca1f1d62a7ea27c70
lidongjie26 <lidongjie26@outlook.com>
2016-11-02 22:45:02 +0800

    update demo

--- ife/ife2016/js/task25.js
+++ ife/ife2016/js/task25.js
@@ -1,86 +1,73 @@
 /**
  * Created by Administrator on 2016/10/8.
+ * 需求1：点击加号，子目录打开，变为减号，再点击减号，子目录关闭，变为加号
+ * 需求2：点击选中某个目录，删除按钮删除，增加按钮则textbox中的文本作为子目录增加到中间去。
+ * 需求3：查询，查询结果换一种效果显示，查询后打开其路径。
+ * 问题1：子节点如何做到缩进比父节点多？
+ * 问题2：点击两次才能打开子节点
  */
-var imageX=true,
-     i,
-    target,
-    textBox=document.getElementsByClassName('textBox')[0],
-    textAdd,
-    divAdd,
-    selectedDiv;
+var i=0;
+var body=document.body;
+var root=document.getElementById('root');
+var nodes=document.getElementsByTagName('div');
+var oA=document.getElementsByTagName('a');
+for(i=0;i<nodes.length;i++){
+    nodes[i].aFlag=true;
+}
+root.firstElementChild.aFlag=false;
 var EventUtil={
-    addHandler:function(element,type,handler){
-        if(element.addEventListener){
-            element.addEventListener(type,handler,false);
-        }else if(element.attachEvent){
-            element.attachEvent('on'+type,handler);
-        }else{
+    addEvent:function(element,type,handler){
             element['on'+type]=handler;
-        }
+    },
+    getEvent:function(event){
+        return event?event:window.event;
     },
     getTarget:function(event){
         return event.target||event.srcElement;
     }
 };
-var body=document.body;
-EventUtil.addHandler(body,'click',function(event){
-    target=EventUtil.getTarget(event);
+EventUtil.addEvent(body,'click',function(event){
+    var target=EventUtil.getTarget(event);
+    var nodeTarget=target.parentNode;
     switch(target.className){
-        case 'image':
-            var selectCatalog=target.parentNode.parentNode;
-            var divSelected=selectCatalog.children;
-            if(imageX){
-                //改为减号，打开
-                target.src='image/minus.ico';
-                imageX=false;
-                console.log(divSelected.length);
-                for(i=0;i<divSelected.length;i++){
-                    if(divSelected[i].tagName=='DIV'){
-                        if(divSelected[i].style.display=='none'){
-                            divSelected[i].style.display='block';
-                        }
-                    }
-                }
-            }else{
-                //改为加号，关闭
-                target.src='image/plus.ico';
-                imageX=true;
-                for(i=0;i<divSelected.length;i++){
-                    if(divSelected[i].tagName=='DIV'){
-                        divSelected[i].style.display='none';
-                    }
-                }
+        case 'display':
+            display(target,nodeTarget.aFlag);
+            //aFlag为true，表示是折叠状态，现在需要打开，
+            if(nodeTarget.aFlag){
+                target.innerHTML='-';
+                nodeTarget.aFlag=false;
+            }
+            else{
+                target.innerHTML='+';
+                nodeTarget.aFlag=true;
             }
             break;
-        case 'listName':
-            clearAll();
-            target.style.backgroundColor='#4169E1';
-            selectedDiv=target;
-            break;
-        case 'btnAdd':
-            textAdd=textBox.value;
-            textAdd=trim(textAdd);
-            if(textAdd!==''){
-                divAdd=document.createElement('div');
-                divAdd.innerHTML='<p class="listName">        <img class="image" src="image/plus.ico">'+textAdd+'</p>';
-                selectedDiv.appendChild(divAdd);
-                divAdd.className='three';
-                console.log(divAdd.parentNode);
-                textBox.value='';
-                textBox.focus();
-            }else{
-                alert('输入为空，请重新输入！');
-                textBox.value='';
-                textBox.focus();
-            }
     }
 });
-function clearAll(){
-    var items=document.getElementsByClassName('listName');
-    for(i=0;i<items.length;i++){
-        items[i].style.backgroundColor='#fff';
+function display(node,flag){
+    var nodeList=[];
+    nextSiblings(node,nodeList);
+    if(flag){
+        for( i=0;i<nodeList.length;i++){
+            nodeList[i].style.display='block';
+            nodeList[i].aFlag=false;
+        }
     }
+    var nodes=node.parentNode.getElementsByTagName('div');
+    for(i=0;i<nodes.length;i++) {
+            if (nodes[i].aFlag) {
+                nodes[i].style.display='none';
+                nodes[i].aFlag=false;
+            } else {
+                nodes[i].style.display = 'block';
+                nodes[i].aFlag=true;
+            }
+    }
+    nodeList=[];
 }
-function trim(str){
-    return str.replace(/^\s+|\s$/g,'');
+function nextSiblings(node,list){
+    if(node){
+        list.push(node);
+        nextSiblings(node.nextElementSibling,list);
+    }
 }
