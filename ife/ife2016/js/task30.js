34cda5c6198d9eb21e691aa804c5c5e983a13aea
李冬杰 <lidongjie26@outlook.com>
2017-01-04 18:06:21 +0800

    1

--- ife/ife2016/js/task30.js
+++ ife/ife2016/js/task30.js
@@ -24,14 +24,119 @@ window.onload=function(){
         var tip=element.nextElementSibling;
         tip.style.visibility='visible';
     }
-    var wrap=document.getElementsByClassName('wrap')[0];
-    wrap.onfocus=function(event){
-        var target=EventUtil.getTarget(event);
-        switch(target.className){
-            case 'textbox':
-                showTip(target);
-                break;
+    function Trim(str){
+        return str.replace(/^[\s,，、\r\t]+|[\s,，、\r\t]$/g,'');
+    }
+    function cal(str){
+        var re=/[\u4E00-\u9FA5]/g;
+        if(re.test(str)){
+            return str.match(re).length;
         }
+        else{
+            return 0;
+        }
+    }
+    var checkResult={
+        tip:'',
+        right:false
     };
+    var inputEles = [$('#name'), $('#password'), $('#comfirm'), $('#email'), $('#tel')];
+    function checkValue(ele){
+        var value=ele.value;
+        value=Trim(value);
+        var len=value.length+cal(value);
+        if(value===''){
+            checkResult.tip='不能为空';
+            checkResult.right=false;
+        }
+        if(ele===inputEles[0]){
+            if(len>=4&&len<=16){
+               checkResult.tip='验证通过';
+                checkResult.right = true;
+            }else{
+                checkResult.tip='长度为4~16个字符';
+                checkResult.right=false;
+            }
+        }
+        if(ele===inputEles[1]){
+            if(len>=4&&len<=16){
+                checkResult.tip='密码可用';
+                checkResult.right = true;
+            }else{
+                checkResult.tip='长度为4~16个字符';
+                checkResult.right=false;
+            }
+        }
+        if(ele===inputEles[2]){
+            if(value===inputEles[1].value.trim()){
+                checkResult.right = true;
+                checkResult.tip = '密码正确';
+            } else {
+                checkResult.right = false;
+                checkResult.tip = '两次密码输入要相同';
+            }
+        }
+        if(ele===inputEles[3]){
+            var szReg=/^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
+            var bChk=szReg.test(value);
+            if(bChk){
+                checkResult.tip='正确';
+                checkResult.right=true;
+            }else{
+                checkResult.tip='邮箱格式错误';
+                checkResult.right=false;
+            }
+        }
+        if(ele===inputEles[4]){
+            var szReg=/^1[34578]\d{9}$/;
+            var bChk=szReg.test(value);
+            if(bChk){
+                checkResult.tip='正确';
+                checkResult.right='true';
+            }else{
+                checkResult.tip='手机号码有误';
+                checkResult.right='false';
+            }
+        }
+    }
+    $('.textbox').blur(function(e){
+        var target=EventUtil.getTarget(e);
+        checkValue(target);
+        var span=target.nextElementSibling;
+        span.innerHTML=checkResult.tip;
+        if(checkResult.right){
+            $(span).css('color','#008000');
+        }else {
+            $(span).css('color','#FF0000');
+        }
+    });
+    $('.textbox').focus(function(e){
+        var target=EventUtil.getTarget(e);
+        showTip(target);
+    });
+    var right=true;
+    $('#submit').click(function(e){
+        for(var i=0;i++;i<inputEles.length){
+            var input=inputEles[i];
+            checkValue(input);
+            showTip(input);
+            var span=input.nextElementSibling;
+            span.innerHTML=checkResult.tip;
+            if (checkResult.right) {
+                $(span).css('color','green');
+            } else {
+                $(span).css('color','green');
+                right = false;
+            }
+
+        }
+        if(right){
+            alert('提交成功');
+        }else {
+            alert('提交失败，请检查输入');
+        }
+    });
 
 };
+
+
