45397ced748dc6c2f32b11f07356bfcd8a23c54f
李冬杰 <lidongjie26@outlook.com>
2017-01-07 15:10:12 +0800

    1

--- ife/ife2016/js/task30.js
+++ ife/ife2016/js/task30.js
@@ -22,10 +22,10 @@ window.onload=function(){
     };
     function showTip(element){
         var tip=element.nextElementSibling;
-        tip.style.visibility='visible';
+        $(tip).css('visibility','visible');
     }
     function Trim(str){
-        return str.replace(/^[\s,，、\r\t]+|[\s,，、\r\t]$/g,'');
+        return str.replace(/^\s+|\s$/g,'');
     }
     function cal(str){
         var re=/[\u4E00-\u9FA5]/g;
@@ -45,11 +45,12 @@ window.onload=function(){
         var value=ele.value;
         value=Trim(value);
         var len=value.length+cal(value);
-        if(value===''){
+        if(value==''){
             checkResult.tip='不能为空';
             checkResult.right=false;
+            return;
         }
-        if(ele===inputEles[0]){
+        if(ele==inputEles[0][0]){
             if(len>=4&&len<=16){
                checkResult.tip='验证通过';
                 checkResult.right = true;
@@ -58,7 +59,7 @@ window.onload=function(){
                 checkResult.right=false;
             }
         }
-        if(ele===inputEles[1]){
+        if(ele==inputEles[1][0]){
             if(len>=4&&len<=16){
                 checkResult.tip='密码可用';
                 checkResult.right = true;
@@ -67,8 +68,8 @@ window.onload=function(){
                 checkResult.right=false;
             }
         }
-        if(ele===inputEles[2]){
-            if(value===inputEles[1].value.trim()){
+        if(ele==inputEles[2][0]){
+            if(value===inputEles[1][0].value.trim()&&value!==''){
                 checkResult.right = true;
                 checkResult.tip = '密码正确';
             } else {
@@ -76,7 +77,7 @@ window.onload=function(){
                 checkResult.tip = '两次密码输入要相同';
             }
         }
-        if(ele===inputEles[3]){
+        if(ele==inputEles[3][0]){
             var szReg=/^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
             var bChk=szReg.test(value);
             if(bChk){
@@ -87,21 +88,22 @@ window.onload=function(){
                 checkResult.right=false;
             }
         }
-        if(ele===inputEles[4]){
+        if(ele==inputEles[4][0]){
             var szReg=/^1[34578]\d{9}$/;
             var bChk=szReg.test(value);
             if(bChk){
                 checkResult.tip='正确';
-                checkResult.right='true';
+                checkResult.right=true;
             }else{
                 checkResult.tip='手机号码有误';
-                checkResult.right='false';
+                checkResult.right=false;
             }
         }
     }
     $('.textbox').blur(function(e){
         var target=EventUtil.getTarget(e);
         checkValue(target);
+        console.log(inputEles[0]);
         var span=target.nextElementSibling;
         span.innerHTML=checkResult.tip;
         if(checkResult.right){
@@ -109,23 +111,28 @@ window.onload=function(){
         }else {
             $(span).css('color','#FF0000');
         }
+        showTip(target);
     });
     $('.textbox').focus(function(e){
         var target=EventUtil.getTarget(e);
         showTip(target);
     });
     var right=true;
-    $('#submit').click(function(e){
-        for(var i=0;i++;i<inputEles.length){
-            var input=inputEles[i];
-            checkValue(input);
+    $('#submit').click(function(){
+        var len=inputEles.length;
+        for(var i=0;i<len;i++){
+            var input=inputEles[i][0];
             showTip(input);
+            checkValue(input);
             var span=input.nextElementSibling;
+            console.log(span);
             span.innerHTML=checkResult.tip;
             if (checkResult.right) {
+                $(input).css('border','2px solid green');
                 $(span).css('color','green');
             } else {
-                $(span).css('color','green');
+                $(input).css('border','2px solid red');
+                $(span).css('color','red');
                 right = false;
             }
 
